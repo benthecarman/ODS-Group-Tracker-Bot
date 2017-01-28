5a598c87f58b19ad8260501dfb0befad5562dce8
Ben Carman <benthecarman@live.com>
2017-01-28 03:38:58 -0600

    Add project files.

---
+++ ODS Group Tracker Bot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ODS_Group_Tracker_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
